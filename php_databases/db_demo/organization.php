4d0634580bd98de4ff34251e08b61fd82fca7ecc
Donal Heidenblad <dheidenb@umd.edu>
2018-10-10 19:32:48 -0400

    Update php_databases/db_demo/organization.php
    Added comments and todos.

--- php_databases/db_demo/organization.php
+++ php_databases/db_demo/organization.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Class Organization: Used to logically bundle all operations related to
+ * organization data.
+ * Implements JsonSerializable so that we can define how we want the object
+ * to be serialized when json_encode is called. JsonSerializable requires that
+ * we define the method jsonSerialize.
+ */
 class Organization implements JsonSerializable {
   public $profile_id;
   public $region;
@@ -14,6 +21,7 @@ class Organization implements JsonSerializable {
   public $size;
   public $type_of_data_used;
 
+  // used to translate our property names to the key names in the JSON file
   private $from_map = array(
     'profileID' => 'profile_id',
     'Region' => 'region',
@@ -32,10 +40,17 @@ class Organization implements JsonSerializable {
 
   private $to_map = array();
 
+  /* constructor defines how an object is created. if we get an array
+   * containing the properties of an organization, then we assign those
+   * values to the appropriate properties
+   */
   function __construct(array $org_array) {
+    // flipping the from_map values
     foreach($this->from_map as $key => $val) {
       $this->to_map[$val] = $key;
     }
+
+    // assigning property values
     foreach ($org_array as $key => $value) {
       if (property_exists($this, $key)) {
         $this->$key = $value;
@@ -43,6 +58,10 @@ class Organization implements JsonSerializable {
     }
   }
 
+  /* called by json_encode to allow us to control how our object is converted
+   * into a json string. we are using it here to convert our internal property
+   * names into the human readable key names used in the JSON file.
+   */
   public function jsonSerialize() {
     $json_assoc = array();
     foreach($this->from_map as $key => $val) {
@@ -51,11 +70,15 @@ class Organization implements JsonSerializable {
     return $json_assoc;
   }
 
+  /* get an organization by id. static because we want to create a new
+   * instance with the data in the database and it doesn't make sense
+   * to create an empty object and then load the data into it...
+   */
   static function fetchOne(mysqli $conn, $profile_id) {
     $stmt = $conn->prepare("SELECT * FROM organizations WHERE profile_id = ?");
     $stmt->bind_param("i", $profile_id);
     $stmt->execute();
-    if ($result = $stmt->get_result()) {
+    if ($result = $stmt->get_result() && $result->num_rows == 0) {
       $org = $result->fetch_assoc();
       return new Organization($org);
     } else {
@@ -63,18 +86,29 @@ class Organization implements JsonSerializable {
     }
   }
 
+  /* get all organizations from db (redundant with fetchMany now)
+   * static because it doesn't make sense to create an instance of the class
+   * in order to return many objects
+   */
   static function fetchAll(mysqli $conn) {
     $orgs = array();
     $sql = 'SELECT * FROM organizations';
     if ($result = $conn->query($sql)) {
       while ($row = $result->fetch_assoc()) {
+        // create a new object from our row data
         $org_obj = new Organization($row);
-
+        // append the created org to the $orgs array
         $orgs[] = $org_obj;
       }
     }
     return $orgs;
   }
+
+  /* get multiple organizations
+   * offset is where to start
+   * limit is the number of results to return
+   * default parameters get all records
+   */
   static function fetchMany(mysqli $conn, $offset = 0, $limit = -1) {
     $stmt = $conn->prepare("SELECT * FROM organizations LIMIT ?, ?");
     $stmt->bind_param("ii", $offset,  $limit);
@@ -82,73 +116,35 @@ class Organization implements JsonSerializable {
     if ($result = $stmt->get_result()) {
       $orgs = array();
       while ($row = $result->fetch_assoc()) {
+        // create a new object from our row data
         $org_obj = new Organization($row);
-
+        // append the created org to the $orgs array
         $orgs[] = $org_obj;
       }
       return $orgs;
     }
   }
 
-  public function save(mysqli $conn) {
-    if ($this->profile_id == 0) {
-      $stmt = $conn->prepare("INSERT INTO organizations
-                              (organization_name, organization_type, region,
-                               country, country_income_level, sectors,
-                               description, city, state_or_region,
-                               founding_year, size, type_of_data_used)
-                              VALUES (?, ?, ?,
-                                      ?, ?, ?,
-                                      ?, ?, ?,
-                                      ?, ?, ?)");
-      $stmt->bind_param("sssssssssiss",
-                        $this->organization_name,
-                        $this->organization_type,
-                        $this->region,
-                        $this->country,
-                        $this->country_income_level,
-                        $this->sectors,
-                        $this->description,
-                        $this->city,
-                        $this->state_or_region,
-                        $this->founding_year,
-                        $this->size,
-                        $this->type_of_data_used);
-    } else {
-      $stmt = $conn->prepare("UPDATE organizations SET
-                              organization_name = ?, organization_type = ?,
-                              region = ?, country = ?, country_income_level  = ?,
-                              sectors = ?, description = ?, city  = ?,
-                              state_or_region = ?, founding_year = ?, size = ?,
-                              type_of_data_used = ?
-                              WHERE profile_id = ?");
-      $stmt->bind_param("sssssssssissi",
-                        $this->organization_name,
-                        $this->organization_type,
-                        $this->region,
-                        $this->country,
-                        $this->country_income_level,
-                        $this->sectors,
-                        $this->description,
-                        $this->city,
-                        $this->state_or_region,
-                        $this->founding_year,
-                        $this->size,
-                        $this->type_of_data_used,
-                        $this->profile_id);
-    }
-    $stmt->execute();
-    $stmt->close();
-  }
-
+  // get the total number of organizations
   static function fetchTotalCount(mysqli $conn) {
     $sql = 'SELECT COUNT(*) FROM organizations';
+    // this query should always return something, but if there is a server issue...
     if ($result = $conn->query($sql)) {
+      // fetch_array with a numerically indexed array is actually handy here...
       $row = $result->fetch_array(MYSQLI_NUM);
       return $row[0];
     } else {
       return 0;
     }
   }
+
+  // TODO #1: Implement a function to save an new organization
+  // return number of rows inserted
+
+  // TODO #2: Implement a function to delete an organization.
+  // return number of rows deleted
+
+  // TODO #3: Implement a function to save changes to an existing organization
+  // return number of rows updated
 }
  ?>
