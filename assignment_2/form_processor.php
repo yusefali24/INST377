dc7c424f96993c0bd3a9fd718f7c1a7f5d5d8aee
Donal Heidenblad <dheidenb@umd.edu>
2018-11-14 13:32:14 -0500

    updated assignment 2 template

---
+++ assignment_2/form_processor.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Team Peer Evaluation - Processor</title>
+
+	<style>
+		div {
+			margin-top: 20px;
+			margin-bottom: 20px;
+		}
+	</style>
+</head>
+<body>
+
+<?php
+// The code to process recieved data from the form goes to here.
+
+?>
+</body>
+</html>
